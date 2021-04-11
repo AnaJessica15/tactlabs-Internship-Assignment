239360c918142500c7ab987a41770ea4fcd5f9d1
AnaJessica15 <82106569+AnaJessica15@users.noreply.github.com>
2021-04-12 00:41:53 +0530

    Add files via upload

---
+++ Flask application.py
@@ -0,0 +1,14 @@
+pip install flask
+pip install flask-pymongo
+pip install dnspython
+from flask import Flask, jsonify, request, redirect
+from flask_pymongo import PyMongo
+app = Flask(__name__)
+app.config["MONGO_URI"] = "<mongodb+srv://aj_15:6385715202@cluster()-jtpxd.mongodb.net/admin>"
+mongo = PyMongo(app)
+#All the routings in our app will be mentioned here.
+@app.route('/test')
+def test():
+    return "App is working perfectly"
+if __name__ == '__main__':
+app.run(debug=True)
