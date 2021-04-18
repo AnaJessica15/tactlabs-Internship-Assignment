a9f9420c1108b7308a463bf6dbd97d9fb973ad7c
AnaJessica15 <82106569+AnaJessica15@users.noreply.github.com>
2021-04-18 19:50:33 +0530

    Update upload(final).py

--- upload(final).py
+++ upload(final).py
@@ -2,24 +2,25 @@
 from flask import Flask,render_template,request
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
+import pymongo
 import os
 
 #Connecting MongoDB to native drive Python
 app = Flask(__name__)
-app.config["MONGO_URI"] ='mongodb+srv://aj_15:*********@cluster0.pdcrn.mongodb.net/aj?retryWrites=true&w=majority'
-mongo = PyMongo(app)
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 #Locating the database and the collection in MongoDB to upload the files
-cluster = MongoClient("mongodb+srv://aj_15:**********@cluster0.pdcrn.mongodb.net/aj?retryWrites=true&w=majority")
-db = cluster["TactInternship"]
-collection = db["TactInternship"]
+MONGO_URI='mongodb://127.0.0.1:5000'
+client = pymongo.MongoClient(MONGO_URI)
+DB = client['TactInternship']
+images = DB.images
 
 #Uploading files via html
 @app.route("/upload_file",methods=["GET","POST"])
 def upload_file():
     if request.method == 'POST':
         for f in request.files.getlist('file_name'):
-            f.save(os.path.join(app.config['MONGO_URI'],f.filename))
+            f.save(os.path.join(APP_ROOT['MONGO_URI'],f.filename))
         return render_template("upload-file.html", msg="File has been uploaded successfully")
     return render_template("upload-file.html", msg="Please choose a file")
 if __name__ == '__main__':
